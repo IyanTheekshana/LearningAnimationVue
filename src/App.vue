7d1e9b4fced48d402d29e1ce05b2cee2b9fb599a
Iyan Theekshana <iyan.dev123@gmail.com>
2023-03-08 09:25:53 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="block"></div>
+    <button>Animate</button>
+  </div>
+  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return { dialogIsVisible: false };
+  },
+  methods: {
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+  background: linear-gradient(318deg, #f75600 0%, #c7e25f 50%, #99f19c 100%);
+  height: 100vh;
+  width: 100%;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #0c0c0c;
+  border-radius: 30px;
+  background-color: #0c0c0c;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #383838;
+  border-color: #383838;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #0c0c0c;
+  margin-bottom: 2rem;
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #0c0c0c;
+  border-radius: 12px;
 }
 </style>
